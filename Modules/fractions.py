0c6e5cc690ca32b76bf666b676b36acb18ebe147
Bartlomiej Ciurus <bartek.ciurus@gmail.com>
2015-11-10 12:55:00 +0100

    next part

--- Modules/fractions.py
+++ Modules/fractions.py
@@ -1,10 +1,13 @@
 __author__ = 'Bartlomiej Ciurus'
 
+
 def is_denominator_valid(fraction):
     return fraction[1] != 0
 
+
 def is_denominator_same(fraction1, fraction2):
     return fraction1[1] == fraction2[1]
+
 
 def add_fraction(fraction1, fraction2):
     if is_denominator_valid(fraction1) and is_denominator_valid(fraction2):
@@ -15,6 +18,7 @@ def add_fraction(fraction1, fraction2):
     print("Zero in denominator!")
     return [0, 0]
 
+
 def sub_fraction(fraction1, fraction2):
     if is_denominator_valid(fraction1) and is_denominator_valid(fraction2):
         if is_denominator_same(fraction1, fraction2):
@@ -24,11 +28,13 @@ def sub_fraction(fraction1, fraction2):
     print("Zero in denominator!")
     return [0, 0]
 
+
 def mul_fraction(fraction1, fraction2):
     if is_denominator_valid(fraction1) and is_denominator_valid(fraction2):
         return [fraction1[0] * fraction2[0], fraction1[1] * fraction2[1]]
     print("Zero in denominator!")
     return [0, 0]
+
 
 def div_fraction(fraction1, fraction2):
     if is_denominator_valid(fraction1) and is_denominator_valid(fraction2):
@@ -36,15 +42,18 @@ def div_fraction(fraction1, fraction2):
     print("Zero in denominator!")
     return [0, 0]
 
+
 def is_positive(fraction):
     is_fraction_positive = (fraction[0] * fraction[1]) >= 0
     return is_denominator_valid(fraction) and is_fraction_positive
+
 
 def is_zero(fraction):
     if is_denominator_valid(fraction):
         return fraction[0] == 0
     print("Zero in denominator!")
     return False
+
 
 def cmp_fractions(fraction1, fraction2):
     if is_denominator_valid(fraction1) and is_denominator_valid(fraction2):
@@ -56,6 +65,6 @@ def cmp_fractions(fraction1, fraction2):
             return 2
     return -1
 
+
 def fraction2float(fraction):
     return fraction[0] / fraction[1]
-
